3b7bec10a60d51685ed8cd902bf706fe45506f05
kleber <kleberjrmmxvi@gmail.com>
2022-08-23 21:36:53 -0300

    rascunho com LISTA

---
+++ bolsa.py
@@ -0,0 +1,8 @@
+data, codigo, quantidade, valor_unit, corretagem = input().split(" ")
+data = int(data)
+codigo = int(codigo)
+quantidade = int(quantidade)
+valor_unit = float(valor_unit)
+corretagem = float(corretagem)
+
+print(data, codigo, quantidade, valor_unit, corretagem)
